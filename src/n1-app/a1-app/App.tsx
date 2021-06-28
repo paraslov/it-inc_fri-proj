df540690106526d15cf03dfc7537bd73a7e52627
paraslov <paraslovjm@gmail.com>
2021-06-28 10:14:30 +0300

    ps_merge-in-test-master .01: initializeApp implemented in App.tsx

--- src/n1-app/a1-app/App.tsx
+++ src/n1-app/a1-app/App.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './App.css'
 import {Routes} from '../a2-routes/Routes'
 import {HashRouter} from 'react-router-dom'
 import {NavBar} from '../../n4-common/components/c1-Navbar/NavBar'
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from 'react-redux'
 import {TAppState} from "../../n2-bll/store";
 import {Preloader} from "../../n4-common/components/c2-Preloader/Preloader";
 import ErrorMessage from "../../n4-common/components/с3-ErrorMessage/ErrorMessage";
+import {initializeApp} from './app_reducer'
 
 export const App = () => {
+    const dispatch = useDispatch()
+
     const isFetching = useSelector<TAppState>(state => state.app.isFetching)
+    const isInitialized = useSelector<TAppState>(state => state.app.isInitialized)
     const error = useSelector<TAppState, string | null>(state => state.app.error)
     console.log(error)
+
+    useEffect(() => {
+        dispatch(initializeApp())
+    }, [])
+
+    // if app is not initialized yet show preloader
+    if(!isInitialized) return <Preloader left={'40%'} top={'40%'} size={'200px'}/>
+
     return (
         <HashRouter>
             <div className="App">
+                {/*why isFetching and preloader here??*/}
                 {isFetching && <Preloader left={'calc(50% - 50px)'}/>}
                 <h6>FP.v.1.08</h6>
                 <NavBar/>
--- src/n1-app/a1-app/app_reducer.ts
+++ src/n1-app/a1-app/app_reducer.ts
@@ -1,15 +1,20 @@
 //* =============================================================== Initial state ===================================>>
 import {TBaseThunk} from '../../n2-bll/store'
+import {loginAPI} from '../../n3-api/loginAPI'
+import {setAuthUserDataAction} from '../../n5-features/f1-login/login_reducer'
 
 const initState = {
     isFetching: false,
     error: null as ErrorMessageType,
+    isInitialized: false,
 }
 
 export const appReducer = (state: TState = initState, action: TAppReducerActions): TState => {
     switch (action.type) {
         case 'para-slov/passwordReducer/SET_IS_FETCHING':
             return {...state, isFetching: action.isFetching}
+        case 'para-slov/passwordReducer/SET_IS_INITIALIZED':
+            return {...state, isInitialized: action.isInitialized}
         case 'para-slov/appReducer/SET_ERROR':
             return {
                 ...state,
@@ -26,15 +31,36 @@ export const appReducer = (state: TState = initState, action: TAppReducerActions
  */
 export const setIsFetching = (isFetching: boolean) =>
     ({type: 'para-slov/passwordReducer/SET_IS_FETCHING', isFetching} as const)
+export const setIsInitialized = (isInitialized: boolean) =>
+    ({type: 'para-slov/passwordReducer/SET_IS_INITIALIZED', isInitialized} as const)
 export const setAppError = (error: string | null) => ({type: 'para-slov/appReducer/SET_ERROR', error} as const)
 
 //* =============================================================== Thunk creators ==================================>>
 
+export const initializeApp = (): TThunk => dispatch => {
+    dispatch(setIsFetching(true))
+    loginAPI.auth()
+        .then(res => {
+            if (res.status === 200) {
+                let {email, _id, name, avatar, publicCardPacksCount} = res.data
+                let isAuth = true
+
+                dispatch(setIsFetching(false))
+                dispatch(setAuthUserDataAction({email, _id, name, avatar, publicCardPacksCount, isAuth}))
+                dispatch(setIsInitialized(true))
+            }
+        }).catch(error => {
+        dispatch(setIsFetching(false))
+    })
+}
 
 //* =============================================================== Types ===========================================>>
 
 export type TState = typeof initState
 export type ErrorMessageType = string | null
-export type TAppReducerActions = ReturnType<typeof setIsFetching> | ReturnType<typeof setAppError>
+export type TAppReducerActions = ReturnType<typeof setIsFetching>
+    | ReturnType<typeof setAppError>
+    | ReturnType<typeof setIsInitialized>
+    | ReturnType<typeof setAuthUserDataAction>
 
 type TThunk = TBaseThunk<TAppReducerActions>
