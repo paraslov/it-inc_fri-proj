d2b6c8ce9bf2fb6d58ca456e583c8fb1266ff348
Naytin <worlddesign1987@gmail.com>
2021-07-04 00:23:03 +0300

    created other requests for the deck of cards

--- src/n3-api/card-decks_api.ts
+++ src/n3-api/card-decks_api.ts
@@ -5,7 +5,32 @@ import {instance} from "./api";
 export const cardDecksAPI = {
     getCards(cardsParams: CardsParams) {
         return instance.get<ResponseCards>('cards/pack', {params: {...cardsParams}})
+    },
+    postCards(cardsData: CreateCardsType) {
+        return instance.post<ResponseCards>('cards/pack',{cardsData})
+    },
+    removeCards(id: string) {
+        return instance.delete('cards/pack', {params: {id}})
+    },
+    updateCards(updateData: UpdateCardType ) {
+        return instance.put('cards/pack',{updateData})
     }
+}
+
+type UpdateCardType = {
+    _id: string
+    name: string
+}
+
+type CreateCardsType = {
+    name: string
+    path?: string
+    grade?: number
+    shots?: number
+    rating?: number
+    deckCover?: string
+    private?: boolean
+    type?: string
 }
 
 type Pack = {
